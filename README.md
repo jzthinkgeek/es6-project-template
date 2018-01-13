101245c2018eeb1df00023cd52316c2c3dc10121
zjydeveloper <zjydeveloper@gmail.com>
2018-01-13 12:33:09 -0800

    Use prettier instead of beautifier; Add Flow

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ES6 Project Template
-This is a ES6 project template which uses Babel, ESLint(Airbnb config), Gulp and JS Beautifier.
+This is a ES6 project template which uses Flow, Babel, ESLint(Airbnb config), Gulp and Prettier.
 
 Recommended IDE: [Visual Studio Code](https://code.visualstudio.com/)
--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@ var gulp = require('gulp');
 var path = require('path');
 var babel = require('gulp-babel');
 var eslint = require('gulp-eslint');
+var flow = require('gulp-flowtype');
 
 var outputPath = path.join(__dirname, 'build', 'out');
 
 function build() {
   return gulp.src(path.join(__dirname, 'src', '**/*.js'))
     .pipe(eslint())
+    .pipe(flow())
     .pipe(babel())
     .pipe(gulp.dest(outputPath));
 }
